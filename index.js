4fe8160948a8c81ff219fbeb09eb608bcbab0780
javohirturgunboyev <javohirboy009@gmail.com>
2024-05-29 16:29:26 +0500

    boshladim

---
+++ index.js
@@ -0,0 +1,41 @@
+// boshladim
+
+// // poyezd kechikkani haqida 1-masala
+// function poyez(a,b){
+//     let yigindi=a+b
+//     if(yigindi<=23){
+//         console.log(yigindi)
+//     }else{
+//         console.log('0')
+//     }
+
+// }
+// poyez(15,5)
+// // birinchi ketish vaqti
+// // ikkinchi poyez kechikan vaqti
+
+// // harorat haqida  2-masala
+
+
+// let selsiy 
+
+// let kelvin = selsiy + 273.15;
+
+// let fahrenheit = (selsiy * 9/5) + 32;
+
+// console.log("Selsiy bo'yicha harorat: " + selsiy + "°C");
+// console.log("Kelvin bo'yicha harorat: " + kelvin + "K");
+// console.log("Farangeyt bo'yicha harorat: " + fahrenheit + "°F");
+
+// // sardor haqidagi 7-masala
+
+// function sardor(a,b){
+//     let z=a/2
+//     let d=b-z
+//     console.log (sardor yana ${d} yashashi kerak)
+// }
+// sardor(50,6)
+// birinchi buvasining yosh
+// ikkinchi sardorning yoshi
+
+// tugadi
